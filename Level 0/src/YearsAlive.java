b2a0934a95c87ba6e478f2cb869ec3d72f936525
rishikhurana2 <league@WTS7.attlocal.net>
2017-05-30 19:59:34 -0700

    i did something cool

---
+++ Level 0/src/YearsAlive.java
@@ -0,0 +1,59 @@
+import javax.swing.JOptionPane;
+
+public class YearsAlive {
+	public static void main(String[] args) {
+		String year=JOptionPane.showInputDialog("What year is it");
+		String born=JOptionPane.showInputDialog("What year were you born in?");
+int intYearBorn=Integer.parseInt(born);
+int intYear=Integer.parseInt(year);
+		int age= intYear-intYearBorn;
+	for (int i = 0; i <age ; i++) {
+		
+	}
+for (int i = intYear; i < age; i--) {
+		
+	}
+for (int i = intYear; i < age; i--) {
+	
+}
+
+	System.out.println("In "+intYear + " you were " + age + " years old in this year");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
+
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
