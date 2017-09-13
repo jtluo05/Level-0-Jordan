11bad510493510df223a09acfe16b1a6b0329601
Oliver-Nizet <league@WTS22.attlocal.net>
2017-09-12 19:55:23 -0700

    fhjfghgmfh

--- Level 0/src/FlamingNinjaStar.java
+++ Level 0/src/FlamingNinjaStar.java
@@ -4,8 +4,9 @@ import org.jointheleague.graphical.robot.Robot;
 
 /*** Teacher's note ***/
 /*
- * Before beginning recipe: 1. ask students to find and explain the variables in this recipe. 2. ask students what robot
- * commands they think they will use to make picture in the laminated hand-outs: http://bit.ly/ninja-star
+ * Before beginning recipe: 1. ask students to find and explain the variables in
+ * this recipe. 2. ask students what robot commands they think they will use to
+ * make picture in the laminated hand-outs: http://bit.ly/ninja-star
  */
 
 public class FlamingNinjaStar {
@@ -17,12 +18,14 @@ public class FlamingNinjaStar {
 		// 1. Make a new robot, and set it's pen down.
 		Robot ninja = new Robot("batman");
 		ninja.penDown();
-		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
+		// *14. Use the methods setX and setY to move the ninja star into the
+		// center of the screen
 
-		// *15. Make some adjustments to see what other kinds of shapes you can make.
+		// *15. Make some adjustments to see what other kinds of shapes you can
+		// make.
 
 		// 12. Set the robot speed to 10
-		ninja.setSpeed(50);
+		ninja.setSpeed(100);
 		// 13. Make all the code below repeat 25 times
 		for (int i = 0; i < 200; i++) {
 
@@ -31,7 +34,8 @@ public class FlamingNinjaStar {
 			// 3. Move the robot 64 pixels
 			ninja.move(64);
 
-			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will
+			// turn the robot counter-clockwise.)
 			ninja.turn(-40);
 			ninja.setPenColor(Color.RED);
 			// 5. Move the robot the distance in the variable flameSize
@@ -45,7 +49,8 @@ public class FlamingNinjaStar {
 			ninja.setPenColor(Color.BLACK);
 			// 9. Move the robot the distance in the variable baseSize
 			ninja.move(baseSize);
-			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+			// 10. Check that your shape is the same as Figure 1. This is one
+			// arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
 		}
 	}
--- Level 0/src/RobotGraffiti.java
+++ Level 0/src/RobotGraffiti.java
@@ -7,14 +7,15 @@ public class RobotGraffiti {
 		Robot r2d2 = new Robot("batman");
 		r2d2.setPenColor(Color.GREEN);
 		r2d2.penDown();
+		r2d2.setPenWidth(10);
 		r2d2.setSpeed(10);
 		r2d2.move(250);
 		r2d2.turn(270);
-		r2d2.move(240);
+		r2d2.move(260);
 		r2d2.turn(180);
-		r2d2.move(480);
+		r2d2.move(520);
 		r2d2.turn(180);
-		r2d2.move(240);
+		r2d2.move(260);
 		r2d2.turn(270);
 		r2d2.move(375);
 		for (int i = 0; i < 180; i++) {
